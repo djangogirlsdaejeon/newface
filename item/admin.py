30c083b8652181d54471b61d96210d4090adc423
송윤섭 <dbstjq91@gmail.com>
2017-07-02 00:27:15 +0900

    modify models

--- item/admin.py
+++ item/admin.py
@@ -4,3 +4,14 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(Category)
+admin.site.register(Drama)
+admin.site.register(DramaEpisode)
+admin.site.register(Item)
+admin.site.register(ItemPhoto)
+admin.site.register(ItemFeedback)
+admin.site.register(ItemComment)
+admin.site.register(ItemReview)
+admin.site.register(Question)
--- item/models.py
+++ item/models.py
@@ -12,29 +12,38 @@ class Category(models.Model):
     db_table = 'category'
 
     name = models.CharField(max_length=100)
+    link = models.CharField(max_length=10, blank=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Drama(models.Model):
     db_table = 'drama'
 
-    category_id = models.ForeignKey(Category)
+    category = models.ForeignKey(Category)
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
 
 class DramaEpisode(models.Model):
     db_table = 'drama_episode'
 
-    drama_id = models.ForeignKey(Drama)
+    drama = models.ForeignKey(Drama)
     name = models.CharField(max_length=200)
     brodcasting_date = models.DateField()
+
+    def __str__(self):
+        return "{drama} {episode}".format(drama=self.drama.name, episode=self.name)
 
 
 class Item(models.Model):
     db_table = 'item'
 
     author = models.ForeignKey(User)
-    drama_id = models.ForeignKey(Drama)
-    episode_id = models.ForeignKey(DramaEpisode, null=True)
+    drama = models.ForeignKey(Drama)
+    episode = models.ForeignKey(DramaEpisode, null=True)
     title = models.CharField(max_length=100)
     content = models.TextField()
     status = models.BooleanField()
@@ -43,7 +52,7 @@ class Item(models.Model):
 class ItemPhoto(models.Model):
     db_table = 'item_photo'
 
-    item_id = models.ForeignKey(Item)
+    item = models.ForeignKey(Item)
     filename = models.CharField(max_length=30)
     path = models.CharField(max_length=200)
 
@@ -51,7 +60,7 @@ class ItemPhoto(models.Model):
 class ItemFeedback(models.Model):
     db_table = 'item_feedback'
 
-    item_id = models.ForeignKey(Item)
+    item = models.ForeignKey(Item)
     author = models.ForeignKey(User)
     value = models.BooleanField()
 
@@ -59,7 +68,7 @@ class ItemFeedback(models.Model):
 class ItemComment(models.Model):
     db_table = 'item_comment'
 
-    item_id = models.ForeignKey(Item)
+    item = models.ForeignKey(Item)
     author = models.ForeignKey(User)
     content = models.TextField()
     
@@ -67,7 +76,7 @@ class ItemComment(models.Model):
 class ItemReview(models.Model):
     db_table = 'item_review'
 
-    item_id = models.ForeignKey(Item)
+    item = models.ForeignKey(Item)
     author = models.ForeignKey(User)
     title = models.CharField(max_length=100)
     content = models.TextField()
@@ -78,8 +87,8 @@ class Question(models.Model):
     db_table = 'question'
 
     author = models.ForeignKey(User)
-    drama_id = models.ForeignKey(Drama, null=True)
-    episode_id = models.ForeignKey(DramaEpisode, null=True)
+    drama = models.ForeignKey(Drama, null=True)
+    episode = models.ForeignKey(DramaEpisode, null=True)
     title = models.CharField(max_length=100)
     content = models.TextField()
 
