9e26ca512b9bcc5a43c41d15899e5c6cbe45a97f
송윤섭 <dbstjq91@gmail.com>
2017-07-01 22:08:22 +0900

    modify models

--- item/models.py
+++ item/models.py
@@ -17,14 +17,24 @@ class Category(models.Model):
 class Drama(models.Model):
     db_table = 'drama'
 
+    category_id = models.ForeignKey(Category)
     name = models.CharField(max_length=100)
-    category = models.ForeignKey(Category)
+
+
+class DramaEpisode(models.Model):
+    db_table = 'drama_episode'
+
+    drama_id = models.ForeignKey(Drama)
+    name = models.CharField(max_length=200)
+    brodcasting_date = models.DateField()
 
 
 class Item(models.Model):
     db_table = 'item'
 
     author = models.ForeignKey(User)
+    drama_id = models.ForeignKey(Drama)
+    episode_id = models.ForeignKey(DramaEpisode, null=True)
     title = models.CharField(max_length=100)
     content = models.TextField()
     status = models.BooleanField()
@@ -45,6 +55,7 @@ class ItemFeedback(models.Model):
     author = models.ForeignKey(User)
     value = models.BooleanField()
 
+
 class ItemComment(models.Model):
     db_table = 'item_comment'
 
@@ -62,9 +73,13 @@ class ItemReview(models.Model):
     content = models.TextField()
     rating = models.FloatField()
 
+
 class Question(models.Model):
     db_table = 'question'
 
     author = models.ForeignKey(User)
+    drama_id = models.ForeignKey(Drama, null=True)
+    episode_id = models.ForeignKey(DramaEpisode, null=True)
     title = models.CharField(max_length=100)
     content = models.TextField()
+
